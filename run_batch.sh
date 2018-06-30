72a839e4888da103497895dd16336eb7f1ea41e2
ssaha6 <31147104+ssaha6@users.noreply.github.com>
2018-06-30 04:35:49 -0500

    running batch in shell

---
+++ run_batch.sh
@@ -0,0 +1,17 @@
+sh run.sh "-a sorcar-first "  &
+sh run.sh "-a sorcar-first -f " &
+sh run.sh "-a sorcar-first -t " &
+sh run.sh "-a sorcar-first -r " &
+sh run.sh "-a sorcar-first -f  -t " &
+sh run.sh "-a sorcar-first -f  -r " &
+sh run.sh "-a sorcar-first -t  -r " &
+sh run.sh "-a sorcar-first -f  -t  -r " &
+
+sh run.sh "-a horndini " &   
+sh run.sh "-a horndini -f " & 
+sh run.sh "-a horndini -t " & 
+sh run.sh "-a horndini -r " &  
+sh run.sh "-a horndini -f  -t " &
+sh run.sh "-a horndini -f  -r " &
+sh run.sh "-a horndini -t  -r " &
+sh run.sh "-a horndini -f  -t  -r "  &
