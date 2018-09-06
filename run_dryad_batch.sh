8fe38111fa07466e156386fa6ff53a1287765fb7
Shambwaditya Saha <ssaha6@illinois.edu>
2018-09-05 21:26:53 -0500

    updated run dryad in batch

--- run_dryad_batch.sh
+++ run_dryad_batch.sh
@@ -1,30 +1,46 @@
-sh run_dryad.sh "-a abstracthoudini" &
+mkdir -p results/dryad
 
-sh run_dryad.sh "-a sorcar "  &
-sh run_dryad.sh "-a sorcar -f " &
-sh run_dryad.sh "-a sorcar -t " &
-sh run_dryad.sh "-a sorcar -r " &
-sh run_dryad.sh "-a sorcar -f  -t " &
-sh run_dryad.sh "-a sorcar -f  -r " &
-sh run_dryad.sh "-a sorcar -t  -r " &
-sh run_dryad.sh "-a sorcar -f  -t  -r " &
-
-sh run_dryad.sh "-a sorcar-first " &
-sh run_dryad.sh "-a sorcar-first -f " &
-sh run_dryad.sh "-a sorcar-first -t " &
-sh run_dryad.sh "-a sorcar-first -r " &
-sh run_dryad.sh "-a sorcar-first -f  -t " &
-sh run_dryad.sh "-a sorcar-first -f  -r " &
-sh run_dryad.sh "-a sorcar-first -t  -r " &
-sh run_dryad.sh "-a sorcar-first -f  -t  -r " &
-
-sh run_dryad.sh "-a horndini " &   
-sh run_dryad.sh "-a horndini -f " & 
-sh run_dryad.sh "-a horndini -t " & 
-sh run_dryad.sh "-a horndini -r " &  
-sh run_dryad.sh "-a horndini -f  -t " &
-sh run_dryad.sh "-a horndini -f  -r " &
-sh run_dryad.sh "-a horndini -t  -r " &
-sh run_dryad.sh "-a horndini -f  -t  -r "  &
-
-
+sh run_dryad.sh "-a horndini " &                                                                                                                                                          
+sh run_dryad.sh "-a horndini -f " &                                                                                                                                                       
+sh run_dryad.sh "-a horndini -t " &                                                                                                                                                       
+sh run_dryad.sh "-a horndini -r " &                                                                                                                                                       
+sh run_dryad.sh "-a horndini -f  -t " &                                                                                                                                                   
+sh run_dryad.sh "-a horndini -f  -r " &                                                                                                                                                   
+sh run_dryad.sh "-a horndini -t  -r " &                                                                                                                                                   
+sh run_dryad.sh "-a horndini -f  -t  -r " &    
+                                                                                                                                           
+sh run_dryad.sh "-a sorcar " &                                                                                                                                                            
+sh run_dryad.sh "-a sorcar -f " &                                                                                                                                                         
+sh run_dryad.sh "-a sorcar -t " &                                                                                                                                                         
+sh run_dryad.sh "-a sorcar -r " &                                                                                                                                                         
+sh run_dryad.sh "-a sorcar -f  -t " &                                                                                                                                                     
+sh run_dryad.sh "-a sorcar -f  -r " &                                                                                                                                                     
+sh run_dryad.sh "-a sorcar -t  -r " &                                                                                                                                                     
+sh run_dryad.sh "-a sorcar -f  -t  -r " &     
+                                                                                                                                            
+sh run_dryad.sh "-a sorcar-first " &                                                                                                                                                      
+sh run_dryad.sh "-a sorcar-first -f " &                                                                                                                                                   
+sh run_dryad.sh "-a sorcar-first -t " &                                                                                                                                                   
+sh run_dryad.sh "-a sorcar-first -r " &                                                                                                                                                   
+sh run_dryad.sh "-a sorcar-first -f  -t " &                                                                                                                                               
+sh run_dryad.sh "-a sorcar-first -f  -r " &                                                                                                                                               
+sh run_dryad.sh "-a sorcar-first -t  -r " &                                                                                                                                               
+sh run_dryad.sh "-a sorcar-first -f  -t  -r " &    
+                                                                                                                                       
+sh run_dryad.sh "-a sorcar-greedy " &                                                                                                                                                     
+sh run_dryad.sh "-a sorcar-greedy -f " &                                                                                                                                                  
+sh run_dryad.sh "-a sorcar-greedy -t " &                                                                                                                                                  
+sh run_dryad.sh "-a sorcar-greedy -r " &                                                                                                                                                  
+sh run_dryad.sh "-a sorcar-greedy -f  -t " &                                                                                                                                              
+sh run_dryad.sh "-a sorcar-greedy -f  -r " &                                                                                                                                              
+sh run_dryad.sh "-a sorcar-greedy -t  -r " &                                                                                                                                              
+sh run_dryad.sh "-a sorcar-greedy -f  -t  -r " &         
+                                                                                                                                 
+sh run_dryad.sh "-a sorcar-minimal " &                                                                                                                                                    
+sh run_dryad.sh "-a sorcar-minimal -f " &                                                                                                                                                 
+sh run_dryad.sh "-a sorcar-minimal -t " &                                                                                                                                                 
+sh run_dryad.sh "-a sorcar-minimal -r " &                                                                                                                                                 
+sh run_dryad.sh "-a sorcar-minimal -f  -t " &                                                                                                                                             
+sh run_dryad.sh "-a sorcar-minimal -f  -r " &                                                                                                                                             
+sh run_dryad.sh "-a sorcar-minimal -t  -r " &                                                                                                                                             
+sh run_dryad.sh "-a sorcar-minimal -f  -t  -r " &      
