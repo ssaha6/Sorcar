07434239d67d340bfc33763ecbb4bc7f24a50920
Shambwaditya Saha <ssaha6@illinois.edu>
2018-09-05 21:24:42 -0500

    run dryad update

--- run_dryad.sh
+++ run_dryad.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# ------------------------------------------------- #
-# Usage: call using 
-# sh run.sh "gpuverify" "-a sorcar -f -t -r"
-# sh run.sh "dryad"     "-a sorcar -f -t -r"
-# sh run.sh "dryad"     "-a abstracthoudini"
-
-
-# ------------------------------------------------- #
-#Sanitize inputs
-
+# call using: sh run_dryad.sh "-a sorcar -f -t -r" 
 SORCAR_OPTION=$1
 if [ -z "$SORCAR_OPTION" ];  then
     exit
@@ -17,64 +8,73 @@ fi
 
 OPTION_KEY=`echo $SORCAR_OPTION | sed -e 's/[ -]//g'`
 
-
 # ------------------------------------------------- #
-#Executable directories
+PREFIX=`pwd`
+export LD_LIBRARY_PATH=$PREFIX/z3/bin:$LD_LIBRARY_PATH
 
-PREFIX=/mnt/d/Docker
 
 BOOGIE_DIR="$PREFIX/boogie_horn_sorcar_false/Binaries/"
-Z3_BIN="$PREFIX/../z3/z3-ubuntu/build/z3"
+Z3_BIN="$PREFIX/z3/bin/z3"
 
-OUTPUT_FILE="$PREFIX/results/dryad/$OPTION_KEY.txt"
-TIMEOUT="10s" 
-
+OUTPUT="$PREFIX/results/dryad/$OPTION_KEY.txt"
+TIMEOUT="600s" 
 
 # ------------------------------------------------- #
-# Setting flags and directories
 
-#Setting source directory
 SOURCELIST="$PREFIX/benchmarks/BenchmarksDryad/phase3invfiles"
 SOURCEDIR="$PREFIX/benchmarks/BenchmarksDryad"
- 
 
-#Setting work directory
-WORK_DIR="$SOURCEDIR/$OPTION_KEY"
-
-
-#Setting prelude file
-ORIGPRELUDEFILE="$SOURCEDIR/Vcc3Prelude.array.bpl"
-PRELUDEFILE="$WORK_DIR/Vcc3Prelude.array.bpl"
-
-
-#Flags
-BOOGIE_OPT=" " #/doNotUseLabels /z3opt:smt.relevancy=0  /doModSetAnalysis "  #" /proverOpt:OPTIMIZE_FOR_BV=true "
- 
-
-USUAL_OPT=" /nologo /env:2 /typeEncoding:m  /mv:- /enhancedErrorMessages:1  /useArrayTheory /prover:SMTLib /noinfer /contractInfer /trace "
 
 
 # ------------------------------------------------- #
-# Set up workspace and copy files 
 
-# Remove old dir
-rm -rf $WORK_DIR 
+#Create workspace
+WORK_DIR="$SOURCEDIR/$OPTION_KEY"
+rm -rf $WORK_DIR
 
-#Copy files
-for file in $(cat $SOURCELIST) ; do
+for file in $(cat $SOURCELIST) ; 
+do
     mkdir -p `dirname $WORK_DIR/$file` 
     cp $SOURCEDIR/$file  $WORK_DIR/$file 
 done
 
+#else
+# WORK_DIR="$SOURCEDIR"
+
 #copy prelude file
-cp $ORIGPRELUDEFILE  $PRELUDEFILE 
-
-#make output directory
-mkdir -p `dirname $OUTPUT_FILE` 
-
+ORIGPRELUDEFILE="$SOURCEDIR/Vcc3Prelude.array.bpl"
+PRELUDEFILE="$WORK_DIR/Vcc3Prelude.array.bpl"
+cp $ORIGPRELUDEFILE $PRELUDEFILE
 
 # ------------------------------------------------- #
-# Run 
+
+BOOGIE_OPT="" #" /doNotUseLabels  /z3opt:smt.relevancy=0  /doModSetAnalysis "  #" /proverOpt:OPTIMIZE_FOR_BV=true "
+ 
+ # #Abstract Houdini Boogie flags
+# BOOGIE_OPT+=" /doNotUseLabels /z3opt:smt.relevancy=0  /doModSetAnalysis "
+# BOOGIE_OPT+=" /proverOpt:OPTIMIZE_FOR_BV=true "
+# BOOGIE_OPT+=" /abstractHoudini:IA[HoudiniConst]  "
+# BOOGIE_OPT+=" /errorLimit:1 "
+
+
+# # #Gpuverify Sorcar Boogie flags
+# BOOGIE_OPT+=" /doNotUseLabels  /z3opt:smt.relevancy=0  /doModSetAnalysis "
+# BOOGIE_OPT+=" /proverOpt:OPTIMIZE_FOR_BV=true "
+# #REMOVE ERRORLIMIT
+# #BOOGIE_OPT+=" /errorLimit:1 "
+
+
+# Dryad Sorcar Boogie flags
+# # BOOGIE_OPT+=" /doNotUseLabels  /z3opt:smt.relevancy=0  /doModSetAnalysis "
+# # BOOGIE_OPT+=" /proverOpt:OPTIMIZE_FOR_BV=true "
+# # #REMOVE ERRORLIMIT
+# # BOOGIE_OPT+=" /errorLimit:1 "
+
+
+USUAL_OPT=" /nologo /env:2 /typeEncoding:m  /mv:-  /enhancedErrorMessages:1  /useArrayTheory /prover:SMTLib /noinfer /contractInfer /trace "
+
+# ------------------------------------------------- #
+
 
 cd $BOOGIE_DIR
 
@@ -84,40 +84,41 @@ do
 echo "#################"
 date
 echo "KEY=$OPTION_KEY"
-echo "OUTPUT_FILE=$OUTPUT_FILE"
+echo "OUTPUT=$OUTPUT"
 
 
-case $OPTION_KEY in
-aabstracthoudini) 
+# case $OPTION_KEY in
+# aabstracthoudini) 
+#     (
+#         time -p  /usr/bin/timeout $TIMEOUT mono ./Boogie.exe  $USUAL_OPT /z3exe:$Z3_BIN  $BOOGIE_OPT  /abstractHoudini:IA[HoudiniConst] $PRELUDEFILE  $WORK_DIR/$file ;
+#     ) ;;
+
+# *) 
     (
-        time -p  /usr/bin/timeout $TIMEOUT mono ./Boogie.exe  $USUAL_OPT /z3exe:$Z3_BIN  $BOOGIE_OPT  /abstractHoudini:IA[HoudiniConst] $PRELUDEFILE  $WORK_DIR/$file ;
-    ) ;;
+        time  /usr/bin/timeout $TIMEOUT mono ./Boogie.exe  $USUAL_OPT /z3exe:$Z3_BIN /mlHoudini:sorcar /learnerOptions:"$SORCAR_OPTION"  $BOOGIE_OPT $PRELUDEFILE $WORK_DIR/$file ;
+    ) 
 
-
-*) 
-    (
-        time -p  /usr/bin/timeout $TIMEOUT mono ./Boogie.exe  $USUAL_OPT /z3exe:$Z3_BIN  $BOOGIE_OPT /mlHoudini:sorcar /learnerOptions:"${SORCAR_OPTION}"  $PRELUDEFILE  $WORK_DIR/$file ;
-    ) ;;
-
-esac
+# esac
 
     # ps  | grep z3       | awk '{print $1}' | xargs --no-run-if-empty -I {} kill -9 {}
     # ps  | grep Boogie   | awk '{print $1}' | xargs --no-run-if-empty -I {} kill -9 {}
     # ps  | grep mono     | awk '{print $1}' | xargs --no-run-if-empty -I {} kill -9 {}  
     # ps  | grep timeout  | awk '{print $1}' | xargs --no-run-if-empty -I {} kill -9 {}
 
+
+
 echo "FILE:$file"
         
-done 2>&1 | tee "$OUTPUT_FILE"
+done 2>&1 | tee $OUTPUT
+
 
 
 
 # ------------------------------------------------- #
-# references
 
 # Sorcar learnerOptions
 # -a <algorithm>        Selects the learning algorithm. Valid options are:
-#                       horndini, sorcar, sorcar-first
+#                    	horndini, sorcar, sorcar-first
 # -f                    Runs Horndini in the first round.
 # -t                    Alternates Horndini and Sorcar between rounds.
 # -r                    Resets the set R in each round.
@@ -125,13 +126,13 @@ done 2>&1 | tee "$OUTPUT_FILE"
 
 #options
 #" -a sorcar -f -t -r ","ftr"
-#" -a sorcar -f -t ",   "ft"
-#" -a sorcar -f -r ",   "fr"
-#" -a sorcar -f ",      "f"
-#" -a sorcar -t -r ",   "tr"
-#" -a sorcar -t ",      "t"
-#" -a sorcar -r ",      "r"
-#" -a sorcar ",         "vanila"
+#" -a sorcar -f -t ", 	"ft"
+#" -a sorcar -f -r ", 	"fr"
+#" -a sorcar -f ", 		"f"
+#" -a sorcar -t -r ",	"tr"
+#" -a sorcar -t ", 		"t"
+#" -a sorcar -r ",		"r"
+#" -a sorcar ", 		"vanila"
 
 
 
