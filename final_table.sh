1fb8a8567dd8daf684bddc12b0dc691445c7034d
Shambwaditya Saha <ssaha6@mx.uillinois.edu>
2019-02-05 14:29:01 -0600

    just greedy

--- final_table.sh
+++ final_table.sh
@@ -1,11 +1,18 @@
 
+
+
+rm results/gpuverify/asorcargreedy.txt
+sh run.sh "-a sorcar-greedy "    
+
+
+
 # Remove old results folder
-rm -rf results
+# rm -rf results
 
 
 # Make directories
-mkdir -p results/gpuverify
-mkdir -p results/dryad
+# mkdir -p results/gpuverify
+# mkdir -p results/dryad
 
 # Check directory
 # Check timeout = 1200s
@@ -15,17 +22,17 @@ mkdir -p results/dryad
 
 # Boogie/Houdini on dryad
 # output: results/dryad/boogie.txt
-sh run_dryad_boogie.sh
+# sh run_dryad_boogie.sh
 
 
 
 #run horndini/sorcar on dryad benchmakrs
 #output: results/dryad/<key>.txt
-sh run_dryad.sh "-a horndini "    
-sh run_dryad.sh "-a sorcar "    
-sh run_dryad.sh "-a sorcar-greedy "    
-sh run_dryad.sh "-a sorcar-minimal "    
-sh run_dryad.sh "-a sorcar-first "    
+# sh run_dryad.sh "-a horndini "    
+# sh run_dryad.sh "-a sorcar "    
+# sh run_dryad.sh "-a sorcar-greedy "    
+# sh run_dryad.sh "-a sorcar-minimal "    
+# sh run_dryad.sh "-a sorcar-first "    
 
 
 
@@ -35,23 +42,23 @@ sh run_dryad.sh "-a sorcar-first "
 
 #boogie/houdni on Gpuverify benchmarks
 # output: results/gpuverify/boogie.txt
-sh run_boogie.sh 
+# sh run_boogie.sh 
 
 
 
 #run horndini/sorcar on gpuverify benchmakrs
 #output: results/gpuverify/<key>.txt
-sh run.sh "-a horndini "    
-sh run.sh "-a sorcar "    
-sh run.sh "-a sorcar-greedy "    
-sh run.sh "-a sorcar-minimal "    
-sh run.sh "-a sorcar-first "    
+# sh run.sh "-a horndini "    
+# sh run.sh "-a sorcar "    
+# sh run.sh "-a sorcar-greedy "    
+# sh run.sh "-a sorcar-minimal "    
+# sh run.sh "-a sorcar-first "    
 
 
 
 # run gpuverify tool on gpuverify benchmarks
 # output: results/gpuverify/vanila_gpuverify.txt
-sh run_gpuverify.sh         
+# sh run_gpuverify.sh         
 
 
 
